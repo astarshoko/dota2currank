e831087dca71520f71419185fef30e749cde98de
astarshoko <ramielshoko7@gmail.com>
2024-12-03 14:33:32 +0700

    quick init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ApiData from "./ApiData";
+import './index.css'; // or './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div style={{ fontFamily: "Arial, sans-serif", margin: "20px" }}>
+      <ApiData />
     </div>
   );
 }
